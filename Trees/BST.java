bb53a940029566057e75ef8a88d97313e9e37088
Kumbam Karthik Reddy <84328259+kumbamkarthik@users.noreply.github.com>
2023-09-11 09:24:55 +0530

    Add files via upload

---
+++ Trees/BST.java
@@ -0,0 +1,137 @@
+package Karthik;
+
+public class BST {
+	public BST() {
+
+	}
+	public class Node {
+		private int value;
+		private Node left;
+		private Node right;
+		private int height;
+		public Node(int value) {
+			this.value = value;
+		}
+		public int getValue() {
+			return value;
+		}
+	}
+	private Node root;
+	public int height(Node node) {
+		if (node == null) {
+			return -1;
+		}
+		return node.height;
+	}
+	public boolean isEmpty() {
+		return root == null;
+	}
+	public void insert(int value) {
+		root = insert(root, value);
+	}
+	public Node insert(Node node, int value) {
+		if (node == null) {
+			node = new Node(value);
+			return node;
+		}
+		if (value < node.value) {
+			node.left = insert(node.left, value);
+		}
+		if (value > node.value) {
+			node.right = insert(node.right, value);
+		}
+		node.height = Math.max(height(node.left), height(node.right)) + 1;
+		return node;
+	}
+	public boolean balanced() {
+		return balanced(root);
+	}
+	public boolean balanced(Node node) {
+		if (node == null) {
+			return true;
+		}
+		return Math.abs(height(node.left) - height(node.right)) <= 1 && balanced(node.left) && balanced(node.right);
+	}
+	public void display() {
+		display(this.root, "Root Node: ");
+	}
+	public void display(Node node, String details) {
+		if (node == null) {
+			return;
+		}
+		System.out.println(details + node.value);
+		display(node.left, "Left child of " + node.value + " : ");
+		display(node.right, "Right child of " + node.value + " : ");
+	}
+	public void populate(int[] nums) {
+		for (int i = 0; i < nums.length; i++) {
+			this.insert(nums[i]);
+		}
+	}
+	
+	public void populateSorted(int nums[]) {
+		populateSorted(nums,0,nums.length);
+	}
+	
+	public void populateSorted(int nums[],int start,int end) {
+		if(start>end) {
+			return;
+		}
+		int mid = (start+end)/2;
+		this.insert(nums[mid]);
+		populateSorted(nums,start,mid);
+		populateSorted(nums,mid+1,end);
+	}
+	
+	public void preOrder() {
+		preOrder(root);
+	}
+	
+	private void preOrder(Node node) {
+		if(node==null) {
+			return;
+		}
+		System.out.print(node.value+" ");
+		preOrder(node.left);
+		preOrder(node.right);
+	}
+	
+	public void inOrder() {
+		inOrder(root);
+	}
+	
+	private void inOrder(Node node) {
+		if(node==null) {
+			return;
+		}
+		inOrder(node.left);
+		System.out.print(node.value+" ");
+		inOrder(node.right);
+	}
+	
+	public void postOrder() {
+		postOrder(root);
+	}
+	
+	private void postOrder(Node node) {
+		if(node==null) {
+			return;
+		}
+		postOrder(node.left);
+		postOrder(node.right);
+		System.out.print(node.value+" ");
+	}
+	
+	
+	public static void main(String args[]) {
+		BST tree = new BST();
+		int nums[] = {5,2,7,1,4,6,9,8,3,10};
+		tree.populate(nums);
+		tree.display();
+		tree.preOrder();
+		System.out.println();
+		tree.inOrder();
+		System.out.println();
+		tree.postOrder();
+	}
+}
